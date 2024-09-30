614bd3384d21ccc44cac82f8f2d35ef8921c6b8d
jungbin97 <ljb5054@gmail.com>
2024-09-30 15:15:12 +0900

    Feat: promotion 서비스 추가
    issues: #13

---
+++ promotion/src/main/java/com/tk/gg/promotion/PromotionApplication.java
@@ -0,0 +1,15 @@
+package com.tk.gg.promotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class PromotionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromotionApplication.class, args);
+    }
+
+}
