e20b50f2105fb9e0a1ff5a603dfd6a1ab5462411
twonezero <dnjsdudtm98@naver.com>
2024-10-01 14:22:49 +0900

    Refactor: application component-scan 지정

--- reservation/src/main/java/com/tk/gg/reservation/ReservationApplication.java
+++ reservation/src/main/java/com/tk/gg/reservation/ReservationApplication.java
@@ -3,15 +3,16 @@ package com.tk.gg.reservation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+@ComponentScan(basePackages = {"com.tk.gg","com.tk.gg.common"})
 public class ReservationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationApplication.class, args);
 	}
-
 }
