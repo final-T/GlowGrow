b70e5ecd28da63060bf4156dc6b9cd04eb22e33b
twonezero <dnjsdudtm98@naver.com>
2024-10-01 11:00:23 +0900

    Refactor: Paging 관련 직렬화 설정

--- reservation/src/main/java/com/tk/gg/reservation/ReservationApplication.java
+++ reservation/src/main/java/com/tk/gg/reservation/ReservationApplication.java
@@ -3,9 +3,11 @@ package com.tk.gg.reservation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class ReservationApplication {
 
 	public static void main(String[] args) {
