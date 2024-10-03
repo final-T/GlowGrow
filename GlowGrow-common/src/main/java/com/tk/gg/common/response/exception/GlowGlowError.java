24e9c0e3fc0831c6cb9513cf2d56d73535929f53
ParkJinWu <onu0311@gmail.com>
2024-10-04 00:43:30 +0900

    Feat : 댓글 관련 공통 예외, 응답 메시지 추가
    Issues : #45

--- GlowGrow-common/src/main/java/com/tk/gg/common/response/exception/GlowGlowError.java
+++ GlowGrow-common/src/main/java/com/tk/gg/common/response/exception/GlowGlowError.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum GlowGlowError {
+    //공통
+    NO_SEARCH_RESULTS(404,"SEARCH_001","검색 결과가 없습니다."),
+
 
     // Auth (인증 관련 에러)
     AUTH_INVALID_CREDENTIALS(404, "AUTH_001", "유효하지 않은 자격 증명입니다."),
@@ -16,6 +19,9 @@ public enum GlowGlowError {
     // Post (게시판 관련 에러)
     POST_NO_EXIST(404,"POST_001","존재하지 않은 게시물입니다."),
     POST_LIKE_UPDATE_FAILED(404, "LIKE_001", "게시물 좋아요 업데이트에 실패했습니다."),
+    COMMENT_NO_EXIST(404,"COMMENT_001","존재하지 않은 댓글입니다."),
+    COMMENT_DELETED(404,"COMMENT_002","삭제된 댓글에는 답글을 작성할 수 없습니다."),
+    COMMENT_DEPTH_EXCEEDED(400,"COMMENT_003","댓글의 깊이 제한을 초과했습니다. 대댓글은 1단계까지만 허용됩니다."),
 
 
     // 프로모션 관련 에러
