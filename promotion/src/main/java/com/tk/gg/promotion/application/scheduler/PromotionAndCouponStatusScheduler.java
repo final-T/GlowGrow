dfcd50ecf528fbbc3a5484694e37d884a0ca9001
jungbin97 <ljb5054@gmail.com>
2024-10-03 14:19:57 +0900

    Feat: 매일 자정 쿠폰 상태 스케줄러 처리
    쿠폰 사용기간이 지나면 상태를 EXPIRED 로 변경
    
    issues: #37

--- promotion/src/main/java/com/tk/gg/promotion/application/scheduler/PromotionAndCouponStatusScheduler.java
+++ promotion/src/main/java/com/tk/gg/promotion/application/scheduler/PromotionAndCouponStatusScheduler.java
@@ -1,5 +1,7 @@
 package com.tk.gg.promotion.application.scheduler;
 
+import com.tk.gg.promotion.domain.enums.CouponStatus;
+import com.tk.gg.promotion.domain.enums.PromotionStatus;
 import com.tk.gg.promotion.infrastructure.repository.PromotionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 @Slf4j
 @RequiredArgsConstructor
-public class PromotionStatusScheduler {
+public class PromotionAndCouponStatusScheduler {
     private final PromotionRepository promotionRepository;
 
     // 매일 자정에 실행
@@ -19,7 +21,17 @@ public class PromotionStatusScheduler {
     @Transactional
     public void updatePromotionStatus() {
         log.info("스케줄러 시작 - 기간이 지난 프로모션을 INACTIVE로 변경합니다.");
-        promotionRepository.updatePromotionStatus();
+        promotionRepository.updatePromotionStatus(PromotionStatus.INACTIVE, PromotionStatus.ACTIVE);
+        log.info("스케줄러 종료 - 상태 변경 완료.");
+    }
+
+    // 매일 자정에 실행
+    // 활성화 기간이 지난 쿠폰 상태를 만료로 변경
+    @Scheduled(cron = "0 0 0 * * *")
+    @Transactional
+    public void updateCouponStatus() {
+        log.info("스케줄러 시작 - 기간이 지난 쿠폰을 EXPIRED로 변경합니다.");
+        promotionRepository.updateCouponStatus(CouponStatus.EXPIRED, CouponStatus.ACTIVE);
         log.info("스케줄러 종료 - 상태 변경 완료.");
     }
 }
