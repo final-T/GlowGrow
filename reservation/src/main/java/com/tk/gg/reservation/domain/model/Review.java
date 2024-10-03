27d187b4883fc7fe74daf57f11e8665575659d9f
twonezero <dnjsdudtm98@naver.com>
2024-10-03 22:22:19 +0900

    Feat: Review 엔티티 작성
    issues: #14

---
+++ reservation/src/main/java/com/tk/gg/reservation/domain/model/Review.java
@@ -0,0 +1,63 @@
+package com.tk.gg.reservation.domain.model;
+
+
+import com.tk.gg.common.jpa.BaseEntity;
+import com.tk.gg.reservation.application.dto.UpdateReviewDto;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Builder
+@Entity
+@Table(name = "p_reviews")
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "reservation_id")
+    private Reservation reservation;
+
+    @Column(nullable = false)
+    private Long reviewerId;
+
+    @Column(nullable = false)
+    private Long targetUserId;
+
+    @Column(nullable = false)
+    private Integer rating;
+
+    @Lob
+    private String content;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Review review = (Review) o;
+        return Objects.equals(id, review.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public void update(UpdateReviewDto dto){
+        this.rating = dto.rating();
+        this.content = dto.content();
+    }
+    public void delete(String deletedBy){
+        this.deletedAt = LocalDateTime.now();
+        this.deletedBy = deletedBy;
+    }
+}
