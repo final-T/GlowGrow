24d92e6eff7c1e96d7d152f446f2d383863a6042
twonezero <dnjsdudtm98@naver.com>
2024-10-04 14:30:42 +0900

    Feat: 반환 메시지, 에러 enum 정의 및 코드 반영

--- reservation/src/main/java/com/tk/gg/reservation/application/service/ReviewService.java
+++ reservation/src/main/java/com/tk/gg/reservation/application/service/ReviewService.java
@@ -1,9 +1,11 @@
 package com.tk.gg.reservation.application.service;
 
 
+import com.tk.gg.common.response.exception.GlowGlowException;
 import com.tk.gg.reservation.application.dto.CreateReviewDto;
 import com.tk.gg.reservation.application.dto.ReviewDto;
 import com.tk.gg.reservation.application.dto.ReviewWithReservationDto;
+import com.tk.gg.reservation.domain.type.ReservationStatus;
 import com.tk.gg.reservation.presentation.request.ReviewSearchCondition;
 import com.tk.gg.reservation.application.dto.UpdateReviewDto;
 import com.tk.gg.reservation.domain.model.Reservation;
@@ -17,6 +19,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
+import static com.tk.gg.common.response.exception.GlowGlowError.*;
+
 //TODO : 유저 Role 에 따른 데이터 검증 필요
 //-> ex) 리뷰 남기는 사람이 제공자인지 사용자였는지 체크(양방향 리뷰이기 때문)
 @RequiredArgsConstructor
@@ -28,6 +32,13 @@ public class ReviewService {
     @Transactional
     public ReviewWithReservationDto createReview(CreateReviewDto dto) {
         Reservation reservation = reservationDomainService.getOne(dto.reservationId());
+        // 예약 상태 검증 -> 예약 체크, 거절, 취소인 경우에는 리뷰 불가
+        if (reservation.getReservationStatus().equals(ReservationStatus.CHECK) ||
+                reservation.getReservationStatus().equals(ReservationStatus.REFUSED) ||
+                reservation.getReservationStatus().equals(ReservationStatus.CANCEL)
+        ){
+            throw new GlowGlowException(REVIEW_CANNOT_FAILED);
+        }
         return ReviewWithReservationDto.from(reviewDomainService.create(dto, reservation));
     }
 
