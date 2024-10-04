dfe02aea2a535b03c11ce239f18f83c3cdfbbc89
twonezero <dnjsdudtm98@naver.com>
2024-10-04 14:31:10 +0900

    Feat: 예약 상태에 따른 리뷰 생성 성공/실패 테스트 코드 작성

---
+++ reservation/src/test/java/com/tk/gg/reservation/application/service/ReviewServiceTest.java
@@ -0,0 +1,106 @@
+package com.tk.gg.reservation.application.service;
+
+import com.tk.gg.common.response.exception.GlowGlowError;
+import com.tk.gg.common.response.exception.GlowGlowException;
+import com.tk.gg.reservation.application.dto.CreateReviewDto;
+import com.tk.gg.reservation.application.dto.ReviewWithReservationDto;
+import com.tk.gg.reservation.domain.model.Reservation;
+import com.tk.gg.reservation.domain.model.Review;
+import com.tk.gg.reservation.domain.model.TimeSlot;
+import com.tk.gg.reservation.domain.service.ReservationDomainService;
+import com.tk.gg.reservation.domain.service.ReviewDomainService;
+import com.tk.gg.reservation.domain.type.ReservationStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ReviewServiceTest {
+
+    @InjectMocks
+    private ReviewService reviewService;
+
+    @Mock
+    private ReviewDomainService reviewDomainService;
+
+    @Mock
+    private ReservationDomainService reservationDomainService;
+
+    private CreateReviewDto validCreateReviewDto;
+    private Reservation validReservation;
+    private Review review;
+    private TimeSlot timeSlot;
+
+    @BeforeEach
+    void setUp() {
+        // 예약자가 리뷰 남기는 것만 setting
+        timeSlot = TimeSlot.builder().id(UUID.randomUUID())
+                .serviceProviderId(2L)
+                .availableDate(LocalDate.parse("2024-10-04", DateTimeFormatter.ofPattern("yyyy-MM-dd")))
+                .availableTime(20)
+                .build();
+        validReservation = Reservation.builder()
+                .id(UUID.randomUUID())
+                .timeSlot(timeSlot)
+                .customerId(1L)
+                .serviceProviderId(2L)
+                .reservationDate(LocalDate.parse("2024-10-04", DateTimeFormatter.ofPattern("yyyy-MM-dd")))
+                .reservationTime(20)
+                .reservationStatus(ReservationStatus.CHECK)
+                .build();
+
+        validCreateReviewDto = CreateReviewDto.builder().reservationId(validReservation.getId())
+                .reviewerId(1L).targetUserId(2L).rating(5).build();
+
+        review = Review.builder().id(UUID.randomUUID()).reservation(validReservation)
+                .reviewerId(1L).targetUserId(2L).rating(5) .build();
+    }
+
+    @Test
+    @DisplayName("리뷰 생성 성공 - 예약 상태가 DONE일 때")
+    void createReviewSuccessWhenReservationStatusIsDone() {
+        // Given
+        validReservation.updateStatus(ReservationStatus.DONE);
+        when(reservationDomainService.getOne(any(UUID.class))).thenReturn(validReservation);
+        when(reviewDomainService.create(any(CreateReviewDto.class), any(Reservation.class)))
+                .thenReturn(review);
+
+        // When
+        ReviewWithReservationDto result = reviewService.createReview(validCreateReviewDto);
+
+        // Then
+
+        verify(reservationDomainService, times(1)).getOne(any(UUID.class));
+        verify(reviewDomainService, times(1)).
+                create(any(CreateReviewDto.class), any(Reservation.class));
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = ReservationStatus.class, names = {"CHECK", "REFUSED", "CANCEL"})
+    @DisplayName("리뷰 생성 실패 - 예약 상태가 CHECK, REFUSED, CANCEL일 때")
+    void createReviewFailWhenInvalidReservationStatus(ReservationStatus invalidStatus) {
+        // Given
+        validReservation.updateStatus(invalidStatus);
+        when(reservationDomainService.getOne(any())).thenReturn(validReservation);
+
+        // Then
+        assertThatThrownBy(() -> reviewService.createReview(validCreateReviewDto))
+                .isInstanceOf(GlowGlowException.class)
+                .hasMessage(GlowGlowError.REVIEW_CANNOT_FAILED.getMessage());
+    }
+
+}
