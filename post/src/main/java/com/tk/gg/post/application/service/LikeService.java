44e49ecf6ae421d622601283b7ca40fa915dcfd9
ParkJinWu <onu0311@gmail.com>
2024-10-06 01:20:35 +0900

    Fix : Like  Application, Domain 서비스 분리
    Issues : #51

--- post/src/main/java/com/tk/gg/post/application/service/LikeService.java
+++ post/src/main/java/com/tk/gg/post/application/service/LikeService.java
@@ -4,9 +4,9 @@ import com.tk.gg.post.application.dto.LikeResponseDto;
 import com.tk.gg.post.domain.model.Like;
 import com.tk.gg.post.domain.model.Post;
 import com.tk.gg.post.domain.repository.LikeRepository;
+import com.tk.gg.post.domain.service.LikeDomainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
@@ -17,21 +17,21 @@ public class LikeService {
 
     private final LikeRepository likeRepository;
     private final PostService postService;
+    private final LikeDomainService likeDomainService;
 
     @Transactional
     public LikeResponseDto toggleLike(UUID postId, Long userId) {
         Post post = postService.getPostById(postId);
 
         Like like = likeRepository.findFirstByPostAndUserId(post, userId)
-                .orElse(new Like(post, userId));
+                .orElseGet(() -> likeDomainService.createLike(post, userId));
 
-        // 좋아요 상태 토글
-        like.toggleLikeStatus();
+        likeDomainService.toggleLikeStatus(like);
         likeRepository.save(like);
 
-        postService.updatePostLikeCount(postId, like.getLikeStatus());
+        boolean likeStatus = likeDomainService.getLikeStatus(like);
+        postService.updatePostLikeCount(postId, likeStatus);
 
-        return new LikeResponseDto(like.getLikeId(),postId, userId, like.getLikeStatus());
+        return new LikeResponseDto(like.getLikeId(), postId, userId, likeStatus);
     }
-
 }
