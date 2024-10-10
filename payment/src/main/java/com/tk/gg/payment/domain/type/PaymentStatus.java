240672e45c08fcb11c50b7559bb7bec71b5d4e9c
ParkJinWu <onu0311@gmail.com>
2024-10-10 23:02:06 +0900

    Feat : 결제 상태값 변경
    Issues : #103

--- payment/src/main/java/com/tk/gg/payment/domain/type/PaymentStatus.java
+++ payment/src/main/java/com/tk/gg/payment/domain/type/PaymentStatus.java
@@ -4,7 +4,8 @@ public enum PaymentStatus {
     REQUESTED("요청"),
     COMPLETED("완료"),
     FAILED("실패"),
-    CANCELED("취소");
+    CANCELED("취소"),
+    REFUND("환불");
 
     private final String description;
 
