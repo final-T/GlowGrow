0bd93b50e3324f6c2f9cdd0beb82eb56b1c50730
ParkJinWu <onu0311@gmail.com>
2024-10-12 16:33:03 +0900

    Post SecurityConfig 추가

---
+++ post/src/main/java/com/tk/gg/post/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.tk.gg.post.infrastructure.config;
+
+import com.tk.gg.security.hooks.SecurityRequestMatcher;
+import com.tk.gg.security.hooks.SecurityRequestMatcherChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityRequestMatcherChain securityRequestMatcherChain() {
+        SecurityRequestMatcherChain matcherChain = new SecurityRequestMatcherChain();
+        matcherChain
+                .add(SecurityRequestMatcher.permitAllOf("/api/auth/**"))
+        ;
+
+        return matcherChain;
+    }
+}
