b21952c131bf2e3edc9197c731d5f794d60e735c
hosu <76641246+junghojune@users.noreply.github.com>
2024-10-14 18:44:59 +0900

    feat : cors 설정
    *이 작동을 안해서 하나하나 입력해줬습니다.
    
    issue : #124

--- GlowGrow-common/src/main/java/com/tk/gg/common/cors/WebConfig.java
+++ GlowGrow-common/src/main/java/com/tk/gg/common/cors/WebConfig.java
@@ -10,6 +10,18 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
                 .allowedOriginPatterns("http://localhost:*") // 모든 출처 허용
+                .allowedOrigins(
+                        "http://13.209.24.74:19090",
+                        "http://13.209.24.74:19091",
+                        "http://13.209.24.74:19092",
+                        "http://13.209.24.74:19093",
+                        "http://13.209.24.74:19094",
+                        "http://13.209.24.74:19095",
+                        "http://13.209.24.74:19096",
+                        "http://13.209.24.74:19097",
+                        "http://13.209.24.74:19098",
+                        "http://13.209.24.74:19099"
+                ) // 모든 출처 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
