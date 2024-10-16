0750b330097471b72b2a796c57f25ecaa1cf3132
twonezero <dnjsdudtm98@naver.com>
2024-10-16 17:38:34 +0900

    Feat: 요청 객체 유효성 검증 에러 핸들러

--- GlowGrow-common/src/main/java/com/tk/gg/common/response/exception/GlobalExceptionHandler.java
+++ GlowGrow-common/src/main/java/com/tk/gg/common/response/exception/GlobalExceptionHandler.java
@@ -2,8 +2,13 @@ package com.tk.gg.common.response.exception;
 
 import com.tk.gg.common.response.ApiUtils;
 import com.tk.gg.common.response.GlobalExceptionResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -17,5 +22,19 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(statusCode).body(response);
     }
 
+    //Request Validation 에 의한 에러 처리
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<GlobalExceptionResponse> ExceptionHandler(MethodArgumentNotValidException e) {
+
+        GlobalExceptionResponse errorResponse = ApiUtils.exception(
+                HttpStatus.BAD_REQUEST.value(),
+                "REQ_VALIDATION",
+                "잘못된 요청입니다."
+        );
+        for (FieldError fieldError : e.getFieldErrors()) {
+            errorResponse.addValidation(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
 }
 
