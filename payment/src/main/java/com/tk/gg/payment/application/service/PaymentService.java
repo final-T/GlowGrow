066394c569f77a782ea7b1eec35758b3e4fd692b
ParkJinWu <onu0311@gmail.com>
2024-10-20 16:20:38 +0900

    Feat : 결제 로직 변경
    Issue : #150

--- payment/src/main/java/com/tk/gg/payment/application/service/PaymentService.java
+++ payment/src/main/java/com/tk/gg/payment/application/service/PaymentService.java
@@ -2,19 +2,24 @@ package com.tk.gg.payment.application.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.tk.gg.common.kafka.coupon.CouponUserResponseDto;
+import com.tk.gg.common.kafka.payment.PaymentReservationResponseDto;
 import com.tk.gg.common.response.exception.GlowGlowError;
 import com.tk.gg.common.response.exception.GlowGlowException;
+import com.tk.gg.payment.application.client.CouponService;
+import com.tk.gg.payment.application.client.ReservationService;
 import com.tk.gg.payment.application.client.UserService;
-import com.tk.gg.payment.application.dto.PaymentFailDto;
-import com.tk.gg.payment.application.dto.PaymentRequestDto;
-import com.tk.gg.payment.application.dto.PaymentSuccessDto;
+import com.tk.gg.payment.application.dto.*;
 import com.tk.gg.payment.domain.model.Payment;
 import com.tk.gg.payment.domain.service.PaymentDomainService;
+import com.tk.gg.payment.domain.type.PaymentStatus;
 import com.tk.gg.payment.infrastructure.config.TossPaymentConfig;
 import com.tk.gg.payment.infrastructure.repository.PaymentRepository;
 import com.tk.gg.security.user.AuthUserInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -25,6 +30,7 @@ import org.springframework.web.client.RestTemplate;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -36,6 +42,8 @@ public class PaymentService {
     private final PaymentDomainService paymentDomainService;
     private final PaymentRepository paymentRepository;
     private final TossPaymentConfig tossPaymentConfig;
+    private final ReservationService reservationService;
+    private final CouponService couponService;
 
 
     @Transactional
@@ -45,7 +53,17 @@ public class PaymentService {
             throw new GlowGlowException(GlowGlowError.USER_NO_EXIST);
         }
 
-        Payment payment = paymentDomainService.createPayment(requestDto, authUserInfo);
+        Payment payment = paymentRepository.findById(requestDto.getPaymentId())
+                .orElseThrow(() -> new GlowGlowException(GlowGlowError.PAYMENT_NO_EXIST));
+
+        // 이미 결제된 정보면 결제 요청 X
+        if(PaymentStatus.COMPLETED.equals(payment.getStatus())){
+            throw new GlowGlowException(GlowGlowError.ALREADY_APPROVED);
+        }
+
+        payment.updateFromDto(requestDto); // 사용자가 [결제 하기] 시 변경 가능성 필드 :  쿠폰, 결제 금액 (쿠폰 적용 시)
+
+        log.info("최종 결제 가격 : {} 할인 가격 : {} , 할인 후 가격 : {}  원래 가격 : {}" , payment.getAmount() , payment.getDiscountAmount() , payment.getDiscountedAmount(), payment.getOriginalAmount());
         return paymentRepository.save(payment);
     }
 
@@ -58,7 +76,6 @@ public class PaymentService {
 
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = getHeaders();
-        //JSONObject params = new JSONObject();
         ObjectMapper objectMapper = new ObjectMapper();
         ObjectNode params = objectMapper.createObjectNode();
         params.put("orderId", orderId);
@@ -69,18 +86,27 @@ public class PaymentService {
             result = restTemplate.postForObject(TossPaymentConfig.URL + paymentKey,
                     new HttpEntity<>(params,headers),
                     PaymentSuccessDto.class);
-            // restTemplate.postForObject() -> Post 요청을 보내고 객체로 결과를 받는다.
 
-        paymentDomainService.completePayment(payment, paymentKey, result.getApprovedAt());
+        paymentDomainService.completePayment(payment, paymentKey, result.getApprovedAt(), result.getMethod());
 
         // 결제 정보 저장
         paymentRepository.save(payment);
+
+        // 쿠폰 사용을 했다면, 쿠폰 사용 Feign 호출
+        if(payment.getCouponId() != null && payment.getStatus() == PaymentStatus.COMPLETED){
+            couponService.useCoupon(payment.getCouponId(), payment.getCustomerId());
+        }
+
         return result;
     }
 
     @Transactional
     public PaymentFailDto tossPaymentFail(String code, String message, String orderId) {
         Payment payment = findPaymentByOrderId(orderId);
+
+        if(PaymentStatus.COMPLETED.equals(payment.getStatus())){
+            throw new GlowGlowException(GlowGlowError.ALREADY_APPROVED);
+        }
 
         paymentDomainService.failPayment(payment, message);
         paymentRepository.save(payment);
@@ -131,5 +157,105 @@ public class PaymentService {
 
         paymentDomainService.cancelPayment(payment, message);
         paymentRepository.save(payment);
+    }
+
+    // 결제 준비 메서드 (provider용)
+    @Transactional
+    public UUID preparePayment(PaymentRequestDto requestDto, AuthUserInfo authUserInfo) {
+
+        // 결제 요청 준비를 하는 사용자(authUserInfo.getId)가 예약의 제공자 Id가 아니라면 예외 발생(reservationService)
+        PaymentReservationResponseDto reservationInfo = reservationService.getOneReservation(requestDto.getReservationId(), authUserInfo.getToken());
+        Long customerId = reservationInfo.customerId(); //예약의 이용자 ID
+        Payment payment = paymentDomainService.createPayment(requestDto, authUserInfo, customerId);
+
+        log.info("@@@@@ customerId : {} & providerId :{} " , reservationInfo.customerId(), reservationInfo.serviceProviderId());
+
+        Payment savedPayment = paymentRepository.save(payment);
+
+        return savedPayment.getPaymentId();
+    }
+
+    public List<CouponUserResponseDto> getUserCoupons(String authToken) {
+        List<CouponUserResponseDto> allCoupons = couponService.getUserCoupons(authToken);
+
+        // 쿠폰이 활성화(ACTIVE)된 쿠폰만 가져옴
+        List<CouponUserResponseDto> activeCoupons = allCoupons.stream()
+                .filter(coupon -> "ACTIVE".equals(coupon.getStatus()))
+                .toList();
+
+        for (CouponUserResponseDto coupon : activeCoupons) {
+            log.info("Coupon: {}", coupon);
+            log.debug("Coupon Details - ID: {}, Promotion ID: {}, Description: {}, Discount Type: {}, " +
+                            "Discount Value: {}, Max Discount: {}, Valid From: {}, Valid Until: {}, Status: {}, Is Used: {}",
+                    coupon.getCouponId(), coupon.getPromotionId(), coupon.getDescription(),
+                    coupon.getDiscountType(), coupon.getDiscountValue(), coupon.getMaxDiscount(),
+                    coupon.getValidFrom(), coupon.getValidUntil(), coupon.getStatus(), coupon.getIsUsed());
+        }
+        return activeCoupons;
+    }
+
+    @Transactional(readOnly = true)
+    public PaymentRequestDto getPreparedPayment(UUID paymentId) {
+        Payment payment = paymentRepository.findByPaymentId(paymentId)
+                .orElseThrow(() -> new GlowGlowException(GlowGlowError.PAYMENT_NO_EXIST));
+
+        return PaymentRequestDto.builder()
+                .userId(payment.getUserId())
+                .customerId(payment.getCustomerId())
+                .paymentId(payment.getPaymentId())
+                .amount(payment.getAmount())
+                .orderName(payment.getOrderName())
+                .reservationId(payment.getReservationId())
+                .status(payment.getStatus())
+                .build();
+    }
+
+    public String getTossClientKey() {
+        return tossPaymentConfig.getClientKey();
+    }
+
+    // 내 결제 조회
+    @Transactional(readOnly = true)
+    public List<PaymentResponseDto.Get> getPayments(AuthUserInfo authUserInfo) {
+        // 사용자 존재 여부 검증
+        boolean userExists = userService.isUserExistsByEmail(authUserInfo.getEmail());
+        if (!userExists) {
+            throw new GlowGlowException(GlowGlowError.USER_NO_EXIST);
+        }
+
+        List<Payment> paymentList = paymentDomainService.getPaymentsByCustomerId(authUserInfo.getId());
+
+        if(paymentList.isEmpty()){
+            throw new GlowGlowException(GlowGlowError.MY_PAYMENT_NOT_FOUND);
+        }
+
+        return paymentList.stream()
+                .map(PaymentResponseDto.Get::from)
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public Page<PaymentResponseDto.Get> searchPayments(AuthUserInfo authUserInfo, Pageable pageable, PaymentSearchCondition condition) {
+        // 사용자 존재 여부 검증
+        boolean userExists = userService.isUserExistsByEmail(authUserInfo.getEmail());
+        if (!userExists) {
+            throw new GlowGlowException(GlowGlowError.USER_NO_EXIST);
+        }
+
+        // 사용자 역할에 따른 접근 제어
+        switch (authUserInfo.getUserRole()) {
+            case MASTER:
+                // MASTER는 모든 결제 내역을 볼 수 있음
+                break;
+            case PROVIDER:
+            case CUSTOMER:
+                // PROVIDER와 CUSTOMER는 자신의 결제 내역만 볼 수 있음
+                condition.setCustomerId(authUserInfo.getId());
+                break;
+            default:
+                throw new GlowGlowException(GlowGlowError.AUTH_UNAUTHORIZED);
+        }
+
+        return paymentDomainService.searchPayments(condition, pageable);
     }
 }
